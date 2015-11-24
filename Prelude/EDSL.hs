a99839f8942e4c315f20c8188e22e2b3c8d88058
Emil Axelsson <emax@chalmers.se>
2015-11-24 18:01:12 +0100

    Add files

---
+++ Prelude/EDSL.hs
@@ -0,0 +1,43 @@
+-- | A subset of the "Prelude" motivated by deeply embedded domain-specific
+-- languages
+
+module Prelude.EDSL
+  ( Bool (..)
+  , Double
+  , Float
+  , Int
+  , IO
+  , Integer
+  , Maybe (..)
+  , String
+
+  , Bounded (..)
+  , Fractional (..)
+  , Functor (..)
+  , Monad (..)
+  , Num (..)
+  , Read (..)
+  , Show (..)
+
+  , (.)
+  , ($)
+  , asTypeOf
+  , const
+  , curry
+  , flip
+  , fst
+  , id
+  , otherwise
+  , print
+  , putStr
+  , putStrLn
+  , readFile
+  , snd
+  , subtract
+  , toInteger
+  , toRational
+  , uncurry
+  , undefined
+  , writeFile
+  ) where
+
